14979ba8456ec2566c50be82524855c754797c37
Krisnia <niasyahwadani@gmail.com>
2024-08-06 15:43:47 +0700

    Fetch(HTML, JS, CSS) : Slicing Purchase Order Supplier

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
