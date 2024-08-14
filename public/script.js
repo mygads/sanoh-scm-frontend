3826378be4faff1b3682a7ccce5ae65fc666631b
myogaads <m.yogaadi1234@gmail.com>
2024-08-14 13:39:43 +0700

    fix : responsive 1080p dashboard login

--- public/script.js
+++ public/script.js
@@ -9,5 +9,11 @@ tailwind.config = {
 }
 
 
-
+// Dummy user data
+const users = [
+    { username: '1', password: '1', role: 'supplier' },
+    { username: '2', password: '2', role: 'admin gudang' },
+    { username: '3', password: '3', role: 'admin purchasing' },
+    { username: '4', password: '4', role: 'super admin' },
+  ];
 
