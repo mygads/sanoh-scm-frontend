c912f2c5a93055a1ec04f828e862496440c4033f
myogaads <m.yogaadi1234@gmail.com>
2024-09-04 12:16:26 +0700

    FEAT: add new loading toogle in login button

--- public/super-admin/script.js
+++ public/super-admin/script.js
@@ -1,6 +1,5 @@
 function generateRandomPassword(maxLength = 10) {
     const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
-    
     const selectedSupplierId = $('#supplier_id').val(); // Ambil bp_code yang dipilih
     
     // Hitung sisa panjang setelah bp_code
@@ -16,67 +15,6 @@ function generateRandomPassword(maxLength = 10) {
     return selectedSupplierId + randomPart; // Password akan dihasilkan berdasarkan bp_code + karakter random
 }
 
-function initializeForm() {
-
-    const roleSelect = document.getElementById('role');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const togglePasswordButton = document.getElementById('toggle_password');
-
-
-    // Email validation
-    emailInput.addEventListener('input', () => {
-      if (!emailInput.value.includes('@')) {
-        document.getElementById('email_error').classList.remove('hidden');
-      } else {
-        document.getElementById('email_error').classList.add('hidden');
-      }
-    });
-
-    // Toggle Password Visibility
-    togglePasswordButton.addEventListener('click', () => {
-      const isPasswordVisible = passwordInput.type === 'text';
-      passwordInput.type = isPasswordVisible ? 'password' : 'text';
-      togglePasswordButton.src = isPasswordVisible ? '../assets/icon_see.svg' : '../assets/icon_hide.svg';
-    });
-
-    // Generate Random Password
-    const generatePasswordButton = document.getElementById('generate_password');
-
-    generatePasswordButton.addEventListener('click', () => {
-      const password = generateRandomPassword();
-      passwordInput.value = password;
-    });
-
-
-    // Form Validation on Submit
-    const form = document.getElementById('user-form');
-
-    form.addEventListener('submit', (e) => {
-      let valid = true;
-
-
-      // Role must be selected
-      if (!roleSelect.value) {
-        valid = false;
-      }
-
-      // Email must contain "@" symbol
-      if (!emailInput.value.includes('@')) {
-        document.getElementById('email_error').classList.remove('hidden');
-        valid = false;
-      } else {
-        document.getElementById('email_error').classList.add('hidden');
-      }
-
-      if (!valid) {
-        e.preventDefault(); // Prevent form submission if validation fails
-      } else {
-
-      }
-    });
-
-};
 
 function handleErrorsPassword(errors) {
   if (errors.password) {
@@ -85,3 +23,4 @@ function handleErrorsPassword(errors) {
     displayNotification('An unknown error occurred.', 'error');
   }
 }
+
